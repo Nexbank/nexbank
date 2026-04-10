7bebd0c155f22963dd156096b71c8c5fb807dbb6
Nqobile <nqobilemagwaza6@gmail.com>
2026-04-10 11:30:20 +0200

    settings page ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./styles/global.css";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Transactions from "./pages/Transactions";
+import Settings from "./pages/Settings";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/transactions" element={<Transactions />} />
+        <Route path="/settings" element={<Settings />} />
       </Routes>
     </Router>
   );

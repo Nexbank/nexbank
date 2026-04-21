e5c4636178e446725379fff3d8701f197ea097f8
Phathisa Ndaliso <phathisandali05@gmail.com>
2026-04-21 22:51:10 +0000

    refactor: align files

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./styles/global.css";
-
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import LandingPage from "./pages/LandingPage";
-
+import ForgotPassword from "./pages/ForgotPassword";
 import Dashboard from "./pages/Dashboard";
+import LandingPage from "./pages/LandingPage";
 import Accounts from "./pages/Accounts";
 import Transactions from "./pages/Transactions";
 import Cards from "./pages/Cards";
 import Insights from "./pages/Insights";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
-
 import Deposit from "./pages/Deposit";
 import Withdraw from "./pages/Withdraw";
 import Transfer from "./pages/Transfer";
 import PayBills from "./pages/PayBills";
+import Profile from "./pages/Profile";
+import Settings from "./pages/Settings";
+import Insights from "./pages/Insights";
 
 import { AccountProvider } from "./context/AccountContext";
 
@@ -26,26 +27,24 @@ function App() {
       <Router>
         <Routes>
 
-          {/* Public routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          {/* Landing page */}
+        <Route path="/" element={<LandingPage />} />
 
-          {/* Main app routes */}
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/accounts" element={<Accounts />} />
-          <Route path="/transactions" element={<Transactions />} />
-          <Route path="/cards" element={<Cards />} />
-          <Route path="/insights" element={<Insights />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/settings" element={<Settings />} />
-
-          {/* Banking actions */}
-          <Route path="/deposit" element={<Deposit />} />
-          <Route path="/withdraw" element={<Withdraw />} />
-          <Route path="/transfer" element={<Transfer />} />
-          <Route path="/pay-bills" element={<PayBills />} />
-
+        {/* Auth pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/accounts" element={<Accounts />} />
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/pay-bills" element={<PayBills />} />
+        <Route path="/insights" element={<Insights />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/settings" element={<Settings />} />
         </Routes>
       </Router>
     </AccountProvider>

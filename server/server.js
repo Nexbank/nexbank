743718b93d2094cd77d3a86fbdaf35aadd6c4c1e
Nozwelo <nicholatenozwelo@gmail.com>
2026-04-22 10:58:51 +0200

    Complete settings page - frontend UI and backend API

--- server/server.js
+++ server/server.js
@@ -2,49 +2,34 @@ const dns = require("dns");
 dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const express = require("express");
-
 const mongoose = require("mongoose");
-
 const cors = require("cors");
-
 require("dotenv").config();
 
 const app = express();
 
 // Middleware
-
 app.use(cors());
-
 app.use(express.json());
 
-// routes
+// Routes
 const authRoutes = require("./routes/authRoutes");
-const profileRoutes = require("./routes/profile");
+const settingsRoutes = require("./routes/settingsRoutes"); // ADD THIS LINE
 
-app.use("/api/profile", profileRoutes);
 app.use("/api/auth", authRoutes);
-
+app.use("/api/settings", settingsRoutes); // ADD THIS LINE
 
 // MongoDB Connection
-
 mongoose
-
   .connect(process.env.MONGO_URI)
-  
-
   .then(() => console.log("✅ MongoDB Connected"))
-
   .catch((err) => console.log(err));
 
 // Test Route
-
 app.get("/", (req, res) => {
   res.send("NexBank API running...");
 });
 
 // Start Server
-
 const PORT = 5000;
-
-
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
